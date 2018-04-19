1cb331bc7af913bb0ebde57236aaf0d41af6028c
fwchen <crystal.tyan@gmail.com>
2018-04-19 10:37:56 +0800

    want to ajust timezone but fail

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,17 +1,40 @@
 import * as winston from 'winston';
+import * as moment from 'moment-timezone';
+
+moment().local();
 
 export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(winston.format.json(), winston.format.timestamp()),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    new winston.transports.File({
+      timestamp: () => {
+        return moment()
+          .tz('Asia/Shanghai')
+          .format('dddd, MMMM Do YYYY, h:mm:ss a');
+      },
+      filename: 'error.log',
+      level: 'error'
+    }),
+    new winston.transports.File({
+      timestamp: () => {
+        return moment()
+          .tz('Asia/Shanghai')
+          .format('dddd, MMMM Do YYYY, h:mm:ss a');
+      },
+      filename: 'combined.log'
+    })
   ]
 });
 
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
+      timestamp: () => {
+        return moment()
+          .tz('Asia/Shanghai')
+          .format('dddd, MMMM Do YYYY, h:mm:ss a');
+      },
       format: winston.format.simple()
     })
   );
