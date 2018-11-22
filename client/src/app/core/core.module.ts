0c75f6c94b531b6fc9ea277211c362e24101a393
fwchen <me@chenfangwei.com>
2018-11-22 20:51:01 +0800

    add core module

---
+++ client/src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './auth.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [
+        AuthService
+      ]
+    };
+  }
+}
