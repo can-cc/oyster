9513025ec01c6268ba175ebe27e576de0d74858c
fwchen <me@chenfangwei.com>
2018-12-09 20:58:54 +0800

    login route

--- server/src/route/auth.route.ts
+++ server/src/route/auth.route.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import * as bcrypt from 'bcryptjs';
 import { getUserByUsername } from '../dao';
 import authService, { AuthService } from '../service/auth.service';
-import {getRepository} from "typeorm";
 
 
 const authRouter = express.Router();
@@ -10,15 +9,11 @@ const authRouter = express.Router();
 authRouter.post('/login', async (req, res, next) => {
   try {
     const { username, password } = req.body;
-    const users = await getUserByUsername(username);
-    if (!users[0]) {
-      return res.status(401).send();
-    }
-    const user = users[0];
-    const match = bcrypt.compareSync(password, user.hash);
-    
-    if (match) {
-      const jwtToken = AuthService.signJwt(user);
+    const user = await authService.login(username, password);
+    if (user) {
+      const jwtToken = AuthService.signJwt({
+        id: user.id
+      });
       return res
         .header('jwt-header', jwtToken)
         .status(200)
--- server/src/service/auth.service.ts
+++ server/src/service/auth.service.ts
@@ -1,19 +1,35 @@
 import * as jwt from 'jsonwebtoken';
 import configure from '../configure';
 import { getRepository } from 'typeorm';
+import { User } from '../entity/User';
+import * as bcrypt from 'bcryptjs';
 
 export class AuthService {
   public static signJwt(data: any) {
-    return jwt.sign(data, configure.getConfig('SERCERT_KEY'));
+    return jwt.sign(
+      {
+        data,
+        exp: configure.getConfig('JWT_EXP')
+      },
+      configure.getConfig('SERCERT_KEY')
+    );
   }
 
   public static unsignJwt(token: string): any {
     return jwt.verify(token, configure.getConfig('SERCERT_KEY'));
   }
 
-  // constructor() {}
-
-  public login(): void {}
+  public async login(username: string, password: string): Promise<User | null> {
+    const user: User = await getRepository(User).findOne({ username });
+    if (!user) {
+      return null;
+    }
+    const match = bcrypt.compareSync(password, user.hash);
+    if (match) {
+      return user;
+    }
+    return user;
+  }
 }
 
 export default new AuthService();
