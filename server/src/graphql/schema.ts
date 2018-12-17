f7265f40ced500908e0b49951aaf49f513c4b934
fwchen <me@chenfangwei.com>
2018-12-17 09:12:16 +0800

    update graphql type

--- server/src/graphql/schema.ts
+++ server/src/graphql/schema.ts
@@ -2,8 +2,12 @@ import { getAtoms } from '../dao';
 const { makeExecutableSchema } = require('graphql-tools');
 
 const typeDefs = `
-  type Query { feeds(limit: Int! offset: Int): [Feed] }
+  type Query { 
+    feeds(limit: Int! offset: Int): [Feed]
+    sources: [FeedSource]
+  }
   type Mutation { source(name: String! url: String!): Boolean }
+  type FeedSource { id: String, name: String, url: String}
   type Feed { id: Int, title: String, author: String, link: String, source: String, content: String, published: Float, isRead: Boolean }
 `;
 
@@ -12,6 +16,9 @@ const resolvers = {
   Query: {
     feeds: async (root, args: { limit: number; offset?: number }, context) => {
       return await getAtoms(args.limit, args.offset);
+    },
+    sources: (root, args: {}) => {
+      return [{id: 1}];
     }
   },
   Mutation: {
