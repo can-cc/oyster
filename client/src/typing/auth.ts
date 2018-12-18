f2ea93c6cd5d6f749c3b16108099b5b6b59fa59e
fwchen <me@chenfangwei.com>
2018-12-18 20:50:36 +0800

    refactor file

--- client/src/typing/auth.ts
+++ client/src/typing/auth.ts
@@ -2,19 +2,3 @@ export declare interface User {
   id: string;
   username: string;
 }
-
-export declare interface FeedSource {
-  id: string;
-  name: string;
-  url: string;
-}
-
-export declare interface Feed {
-  id?: number;
-  title: string;
-  source?: string;
-  link: string;
-  content: string;
-  author: string;
-  published: number;
-}
