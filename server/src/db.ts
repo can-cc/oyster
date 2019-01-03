4c6495f27662ab60958b5e647e49b73ea14156aa
fwchen <me@chenfangwei.com>
2019-01-03 21:07:57 +0800

    remove old db

--- server/src/db.ts
+++
@@ -1,7 +0,0 @@
-import * as path from 'path';
-
-export const knex = require('knex')({
-  client: 'sqlite3',
-  connection: { filename: path.join(__dirname, '../../db/db.sqlite3') },
-  useNullAsDefault: true
-});
