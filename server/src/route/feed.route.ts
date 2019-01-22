58dcb28c13cf7bf838aa9e6f0f150a2b7f6d8efc
fwchen <me@chenfangwei.com>
2019-01-23 00:10:09 +0800

    refactor a action

--- server/src/route/feed.route.ts
+++ server/src/route/feed.route.ts
@@ -47,7 +47,6 @@ feedRouter.post('/api/feed/:feedId/favorite', async (req, res, next) => {
 
 feedRouter.delete('/api/feed/:feedId/favorite/:markId', async (req, res, next) => {
   try {
-    const feedId: string = req.params.feedId;
     const markId: string = req.params.markId;
     const userId: number = req.auth.id;
     await feedMarkerService.removeFeedFavoriteMark({ userId, markId });
