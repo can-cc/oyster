202d138f0537d20d379a3a645828d39b043f52aa
fwchen <me@chenfangwei.com>
2019-05-05 20:38:58 +0800

    initial feed list toolbar

--- client/src/app/feed-list/feed-list-toolbar/feed-list-toolbar.component.ts
+++ client/src/app/feed-list/feed-list-toolbar/feed-list-toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-feed-list-toolbar',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed-list-toolbar.component.css']
 })
 export class FeedListToolbarComponent implements OnInit {
+  faSearch = faSearch;
+
   constructor() {}
 
   ngOnInit() {}
