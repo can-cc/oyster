29482eb9541b93295fea6a37801b75cfdb4fcf18
fwchen <me@chenfangwei.com>
2019-06-17 23:41:19 +0800

    input component

--- client/src/app/component/input/input.component.ts
+++ client/src/app/component/input/input.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+
+export const faIconMap = {
+  faLink: faLink
+};
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => InputComponent)
+    }
+  ]
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, ControlValueAccessor {
+  @Input() icon: string;
 
-  constructor() { }
+  control = new FormControl('');
+  iconMap = faIconMap;
 
-  ngOnInit() {
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  registerOnChange(fn) {
+    this.onChange = fn;
   }
 
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: string): void {
+    this.control.setValue(value);
+  }
 }
