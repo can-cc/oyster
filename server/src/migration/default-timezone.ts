b986a9905bad36d25aab2bbb0790c07f5fe27748
fwchen <me@chenfangwei.com>
2020-03-14 21:00:29 +0800

    fix database migration use demo dbname

--- server/src/migration/default-timezone.ts
+++ server/src/migration/default-timezone.ts
@@ -1,9 +1,10 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import configure from '../configure';
 
 export class DefaultTimeZone1583072848326 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<any> {
     console.log('[database migration] set database default timezone utc');
-    await queryRunner.query(`ALTER DATABASE postgres SET timezone TO 'UTC';`);
+    await queryRunner.query(`ALTER DATABASE ${configure.getConfig('POSTGRES_DB')} SET timezone TO 'UTC';`);
   }
 
   async down(queryRunner: QueryRunner): Promise<any> {}
